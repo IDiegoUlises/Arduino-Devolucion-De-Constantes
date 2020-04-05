9121a60b9064f44c435ff8da0f37ddbf3ea27cb2
Diego Ulises <51557342+IDiegoUlises@users.noreply.github.com>
2020-04-05 16:05:58 -0400

    Update Program.ino

--- Arduino/Program.ino
+++ Arduino/Program.ino
@@ -1,5 +1,3 @@
-```c++
-
 void setup()
 {
   Serial.begin(9600);
@@ -7,7 +5,7 @@ void setup()
 
 void loop()
 {
-  Serial.println(HIGH);
+  Serial.println(HIGH); //imprime el valor de una constante
 }
 
-```
+//Con este codigo se puede obtener la devolucion de una constante
